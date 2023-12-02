7d31b0d21ef05d50767c6f47595af884ffca03ee
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2023-12-02 23:38:03 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,24 +2,24 @@
 using System.IO;
 using System.Reflection;
 
-Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
-{
-    if (args.Name.StartsWith("CefSharp"))
-    {
-        string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
-        string architectureSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
-            Environment.Is64BitProcess ? "x64" : "x86",
-            assemblyName);
+// Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+// {
+//     if (args.Name.StartsWith("CefSharp"))
+//     {
+//         string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+//         string architectureSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+//             Environment.Is64BitProcess ? "x64" : "x86",
+//             assemblyName);
 
-        return File.Exists(architectureSpecificPath)
-            ? Assembly.LoadFile(architectureSpecificPath)
-            : null;
-    }
+//         return File.Exists(architectureSpecificPath)
+//             ? Assembly.LoadFile(architectureSpecificPath)
+//             : null;
+//     }
 
-    return null;
-}
+//     return null;
+// }
 
-AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+// AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
 
 using var game = new cefsharptesting.Game1();
 game.Run();
